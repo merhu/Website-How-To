48a5e210bb2597303c5812ca8b488217ce2e3e22
merhu <merryamba@gmail.com>
2019-07-08 16:56:58 -0400

    working

---
+++ src/main/java/com/example/creatingwebform/CreatingwebformApplication.java
@@ -0,0 +1,13 @@
+package com.example.creatingwebform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingwebformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatingwebformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/creatingwebform/CreatingwebformApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.creatingwebform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreatingwebformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
