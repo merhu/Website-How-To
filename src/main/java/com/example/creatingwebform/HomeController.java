15c9ee1fa270cb7bd2bd8696e38065236d20b2e9
merhu <merryamba@gmail.com>
2019-07-09 17:02:42 -0400

    still working

--- src/main/java/com/example/creatingwebform/HomeController.java
+++ src/main/java/com/example/creatingwebform/HomeController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     @RequestMapping("/index")
-    public String homePage(){
+    public String homePage() {
         return "index";
     }
-
+@RequestMapping ("/catalog")
+    public String catalogPage(){
+        return "Catalog";
+    }
+@RequestMapping ("/pricing")
+    public String pricePage(){
+        return "Pricing";
+    }
 }
